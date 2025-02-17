5c440c788e7d5d4f94b8280d1425ee19b150399f
takuya-fukumura <blackchoco1114@gmail.com>
2025-02-17 23:58:22 +0900

    add 使用例

--- src/main/java/com/example/visuasset/controller/VisuassetController.java
+++ src/main/java/com/example/visuasset/controller/VisuassetController.java
@@ -1,12 +1,12 @@
 package com.example.visuasset.controller;
 
+import com.example.visuasset.entity.AnnualAssets;
+import com.example.visuasset.service.VisuassetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.example.visuasset.service.VisuassetService;
 
 @Controller
 @RequestMapping("/") // URLとの関連付け http://localhost:8080/ の時に呼ばれる
@@ -26,6 +26,9 @@ public class VisuassetController {
 
     @GetMapping("yearly")
     public String yearly(Model model) {
+        AnnualAssets annualAssets = service.getAssetsByYear(2024)
+                .orElseThrow(() -> new RuntimeException("データが見つかりません"));
+        System.out.println(annualAssets.getCash().toString()); // TODO:削除する使用例
         model.addAttribute("message", service.getMessage());
         return "yearly";
     }
