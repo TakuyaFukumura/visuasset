9fa83c35538b7b95b789dbf116ea01fe4f6d2931
takuya-fukumura <blackchoco1114@gmail.com>
2025-02-17 23:48:04 +0900

    add service メソッド

--- src/main/java/com/example/visuasset/service/VisuassetService.java
+++ src/main/java/com/example/visuasset/service/VisuassetService.java
@@ -1,11 +1,26 @@
 package com.example.visuasset.service;
 
+import com.example.visuasset.entity.AnnualAssets;
+import com.example.visuasset.repository.AnnualAssetsRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Slf4j
 @Service
 public class VisuassetService {
+
+    private final AnnualAssetsRepository repository;
+
+    public VisuassetService(AnnualAssetsRepository repository) {
+        this.repository = repository;
+    }
+
+    // 指定した年の資産データを取得（存在しない場合は Optional.empty()）
+    public Optional<AnnualAssets> getAssetsByYear(int year) {
+        return repository.findByTargetYear(year);
+    }
 
     public String getMessage() {
         log.info("getMessage was called"); // ログ出力例
